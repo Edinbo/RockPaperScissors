a1779aa51e4a96bef9bc8ad35232c4bb44acdb4a
Enes <121829291+Edinbo@users.noreply.github.com>
2023-08-19 16:21:56 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,44 +1,57 @@
-#rock paper scissor game
 import random
 
 variables = ["rock", "paper", "scissors"]
-computer_choice = random.choice(variables)
-player_choice = 0
+player_choice = None
 
 def Game():
     player_choice = input("Which move are you going to do? : ")
+    computer_choice = random.choice(variables)
     try:
         if str(computer_choice).lower() == str(player_choice).lower():
             print(f"Computer : {computer_choice}")
             print(f"You : {player_choice}")
             print("Draw!")
+
         elif str(computer_choice).lower() == "rock" and str(player_choice).lower() == "scissors":
             print(f"Computer : {computer_choice}")
             print(f"You : {player_choice}")
             print("Computer WINS!")
-        elif str(computer_choice).lower() == "paper" and str(player_choice).lower() == "scissors":
-            print(f"Computer : {computer_choice}")
-            print(f"You : {player_choice}")
-            print("You WIN!")
-        elif str(computer_choice).lower() == "scissors" and str(player_choice).lower() == "rock":
-            print(f"Computer : {computer_choice}")
-            print(f"You : {player_choice}")
-            print("You WIN!")
+
         elif str(computer_choice).lower() == "rock" and str(player_choice).lower() == "paper":
             print(f"Computer : {computer_choice}")
             print(f"You : {player_choice}")
             print("You WIN!")
+
+        elif str(computer_choice).lower() == "paper" and str(player_choice).lower() == "scissors":
+            print(f"Computer : {computer_choice}")
+            print(f"You : {player_choice}")
+            print("You WIN!")
+
+        elif str(computer_choice).lower() == "paper" and str(player_choice).lower() == "rock":
+            print(f"Computer : {computer_choice}")
+            print(f"You : {player_choice}")
+            print("Computer WIN!")
+
+        elif str(computer_choice).lower() == "scissors" and str(player_choice).lower() == "rock":
+            print(f"Computer : {computer_choice}")
+            print(f"You : {player_choice}")
+            print("You WIN!")
+
+        elif str(computer_choice).lower() == "scissors" and str(player_choice).lower() == "paper":
+            print(f"Computer : {computer_choice}")
+            print(f"You : {player_choice}")
+            print("Computer WIN!")
+
         else:
             print("Invalid Input : You need to input Rock, Paper or Scissors!")
             Game()
     except Exception:
         print("You need to input Rock, Paper or Scissors!")
         Game()
+    play_again = input("Rematch? (yes/no) : ").lower()
+    if play_again == "yes":
+        Game()
+    else:
+        exit()
 
 Game()
-
-play_again = input("Rematch? (yes/no) : ").lower()
-if play_again == "yes":
-    Game()
-else:
-    exit()
